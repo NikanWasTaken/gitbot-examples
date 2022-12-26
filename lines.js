d11f8064929451979405446d953a6b0abda2a1f0
Nikan <83536827+NikanWasTaken@users.noreply.github.com>
2022-12-26 18:20:54 +0330

    feat: create lines.js

---
+++ lines.js
@@ -0,0 +1,9 @@
+const one = "The first line!";
+const two = "This is the second line!!";
+
+// We'll leave the third and fifth line empty!
+
+const six = "6 already!!!";
+
+const eight = "We're already here, wow!!\nThe next one will be nine.";
+const ten = "But this isn't nine??!!";
